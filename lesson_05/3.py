ef478b9dda50ba307bcffae6abe6eced37586faf
Alexei <robinbobin35@gmail.com>
2024-10-28 00:10:56 +0400

    Lesson 05.4

--- lesson_05/3.py
+++ lesson_05/3.py
@@ -5,4 +5,9 @@
 ✔ Далее выведите первые 5 пар ключ-значение,
 обращаясь к итератору, а не к словарю.
 """
+text = 'abrakadabra'
+text_dict_iter = iter({ch: ord(ch) for ch in set(text)}.items())
 
+for i in range(5):
+    key, value = tuple(next(text_dict_iter))
+    print(f'{key = } --> {value = }')
