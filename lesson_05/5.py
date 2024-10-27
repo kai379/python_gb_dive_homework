de43bef7d1bc5740e64e2ffdd60a46c06f576407
Alexei <robinbobin35@gmail.com>
2024-10-28 00:44:11 +0400

    Lesson 05.5

--- lesson_05/5.py
+++ lesson_05/5.py
@@ -7,4 +7,6 @@
 ✔ *Превратите решение в генераторное выражение.
 """
 
-
+fizz_buzz = ('FizzBuzz' if i % 3 == 0 and i % 5 == 0 else 'Fizz' if i % 3 == 0 else 'Buzz' if i % 5 == 0 \
+    else i for i in range(1, 101))
+print(*fizz_buzz, sep='\n')
